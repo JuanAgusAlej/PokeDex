2f5dbbda3f5f5eac41d6c89ffb6a89ee53443c4e
Juan Agustin Alejandro <juan_agustin_alejandro@hotmail.com>
2022-03-07 23:25:34 -0300

    se cambio el loguin

--- src/components/Cargando.jsx
+++ src/components/Cargando.jsx
@@ -1,28 +1,17 @@
 import React from "react";
 import "../css/cargando.css";
+import pokeball from "../img/pokeball.png"
 
 function Cargando() {
     return (
-        <div className="conteiner d-flex align-center">
+       
             
-    <div className="windows8 ">
-      <div className="wBall" id="wBall_1">
-        <div className="wInnerBall"></div>
-      </div>
-      <div className="wBall" id="wBall_2">
-        <div className="wInnerBall"></div>
-      </div>
-      <div className="wBall" id="wBall_3">
-        <div className="wInnerBall"></div>
-      </div>
-      <div className="wBall" id="wBall_4">
-        <div className="wInnerBall"></div>
-      </div>
-      <div className="wBall" id="wBall_5">
-        <div className="wInnerBall"></div>
-      </div>
-            </div>
-            </div>
+        <div class="overlay-loader">
+        <div class="loader">
+            <img src={pokeball}></img>
+        </div>
+    </div>
+            
   );
 }
 
